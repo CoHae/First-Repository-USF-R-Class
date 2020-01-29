e4e68394b144d35cf8cfe5e99a155c37c47a9ccd
Cornelia Haerle <Chaerle@mail.usf.edu>
2020-01-29 09:14:05 -0500

    The data set given represents - or wants to represent - 3 vectors. Each vector has a problem. 1.) The "Name" vector has a comma missing between the vector elements "Marco" and "Carly" which leads to an error message: Once the comma is added (and I corrected the spelling mistake in Bernie's name), the vector is accepted. 2.) The "ABC political poll results" vector has 2 problems: i) it doesn't follow naming conventions (the name has to be one single, continuous string) which leads to an error ii) there is a comma missing between the elements "62"and "51" which leads to an error. Once the name is corrected and the comma is added, the vector is accepted. 3.)The "CBS political poll results" vector has the same naming convention problems as the "ABC political poll results" vector. Once corrected it is accepted.
    Generally I would suggest to make object names shorter than the ones given, for example "ABC_poll".It is still descriptive but faster and easier to write and less opportunity for spelling mistakes.
    
    In order to make these 3 vectors into a Data Frame, you would use the data.frame command.
    Interestingly when making (horizontal) vectors into a data frame, they get "converted" into a (vertical) column of the data frame. Therefore in a data frame each column represents a vector. The different columns of a data frame can have different "modes" (variable types). In a data frame each column represents a "property" (Name or Polling result). Each line represents an "instance" (here the presidential candidates).

--- First Code.R
+++ First Code.R
@@ -1,5 +1,34 @@
 # First-Repository-USF-R-Class
 
+# original/faulty Assignment 2
+# it shows that neither "assignment" nor "someData" exist
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
+myMean(assignment2)
+someData
+# corrected code for Assignment 2
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
 myMean(assignment2)
+
+# Assignment 3
+# original/faulty Assignment 3
+#Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
+#ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+#CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+# commas missing and not allowed spaces in vector names leads to the error messages
+# Error: unexpected input in Name <- c("Jeb", “"
+# Error: unexpected symbol in "CBS political"
+
+# corrected code for Assignment 3
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_political_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_political_poll_results <- c(12, 75, 43, 19, 1, 21, 19) 
+Name
+ABC_political_poll_results
+CBS_political_poll_results
+
+#To make these vectors into a data frame use
+Presidential_Election <- data.frame(Name, ABC_political_poll_results,CBS_political_poll_results)
+Presidential_Election
